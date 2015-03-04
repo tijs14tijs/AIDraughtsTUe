0b4659c8fce9f8843638c8c1e13478d3f810f6b7
Tijs Maas <johanmaas@wlan-238098.nbw.tue.nl>
2015-03-04 14:24:26 +0100

    bugfix

--- src/nl/tue/s2id90/group06/HeuristicDeterminator.java
+++ src/nl/tue/s2id90/group06/HeuristicDeterminator.java
@@ -1,7 +1,5 @@
 package nl.tue.s2id90.group06;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import nl.tue.s2id90.draughts.DraughtsState;
 import static nl.tue.s2id90.draughts.DraughtsState.*;
 
@@ -10,20 +8,10 @@ import static nl.tue.s2id90.draughts.DraughtsState.*;
  * @author Jordie Kolkman & Tijs Maas
  */
 public class HeuristicDeterminator {
-    // If we have seen a state before in the tree, then we store its heuristic.
-    private final HashMap<Integer, Integer> heuristicCache;
     private final boolean isWhite;
-    private int cacheErr = 0;
-    private int cacheGood = 0;
             
     public HeuristicDeterminator(DraughtsState s) {
         isWhite = s.isWhiteToMove();
-        heuristicCache = new HashMap();
-    }
-    
-    // Don't worry, we clear the heuristics cache every turn.
-    public void clearCache() {
-        heuristicCache.clear();
     }
        
     // Determine the heuristic value for a certain state s
@@ -31,23 +19,13 @@ public class HeuristicDeterminator {
         int[] pieces = s.getPieces();
         pieces[0] = depth; // Add depth to the first place of the array: the first place used to be 0 all the time.
         
-        // Even though caching these values might give hash collisions, it speeds up the algorithm a lot so it is worth it.
-        int hashcode = Arrays.hashCode(pieces);
-        Integer cacheResult = heuristicCache.get(hashcode);
-        
+        // This is where the actual heuristic is calculated.
         int h = 0;
-        if(cacheResult == null) {
-            // This is where the actual heuristic is calculated.
-            for (int i = 1; i < pieces.length; i++) {
-                int piece = pieces[i];
-                
-                h += ((piece == WHITEPIECE) ? 2 : 0) - ((piece == BLACKPIECE) ? 1 : 0) + 
-                        ((piece == WHITEKING) ? 3 : 0) - ((piece == BLACKKING) ? 4 : 0);
-            }
-            // Store this value in our cache so that we do not have to calculate it every time.
-            heuristicCache.put(hashcode, h);
-        } else {
-            h = cacheResult;
+        for (int i = 1; i < pieces.length; i++) {
+            int piece = pieces[i];
+
+            h += ((piece == WHITEPIECE) ? 2 : 0) - ((piece == BLACKPIECE) ? 1 : 0) + 
+                    ((piece == WHITEKING) ? 3 : 0) - ((piece == BLACKKING) ? 4 : 0);
         }
 
         // Turn the heuristic around if we are black
