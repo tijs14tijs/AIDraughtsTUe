6bb5ca32509b9274de36a2f1a053e119b940ae65
Tijs Maas <johanmaas@wlan-238098.nbw.tue.nl>
2015-03-04 15:58:19 +0100

    Added old/new algos

---
+++ src/nl/tue/s2id90/group06/AlphaBetaPlayerOld.java
@@ -0,0 +1,121 @@
+package nl.tue.s2id90.group06;
+
+import java.util.List;
+import nl.tue.s2id90.draughts.DraughtsState;
+import nl.tue.s2id90.draughts.player.DraughtsPlayer;
+import org10x10.dam.game.Move;
+
+/**
+ *
+ * @authors Jordie Kolkman & Tijs Maas
+ */
+public class AlphaBetaPlayerOld extends DraughtsPlayer {
+
+    private int INF = Integer.MAX_VALUE;
+    // Warning: bestMove may be used with concurrency, use synchonized methods below to access this variable.
+    private Move bestMove;
+    private boolean stopped;
+    private HeuristicDeterminatorOld heuristics;
+    
+    @Override
+    public Move getMove(DraughtsState s) {
+        stopped = false;
+        // Initialize heuristics calculator or just clear its cache if it already exists.
+        if(heuristics == null) {
+            heuristics = new HeuristicDeterminatorOld(s);
+        }
+        
+        // Iterative deepening, stops immediately when stop() is called
+        try {
+            for (int maxdepth = 7; maxdepth < 30; maxdepth += 1) {
+                // Always start tree with alpha (heuristic adapts to this by being inverted when player is black).
+                int a = Alpha(s, -INF, INF, maxdepth, true);
+                System.out.println("depth:" + maxdepth + ", a:" + a);
+            }
+        // Just in case that we get an exception, we want to keep playing.
+        } catch(AIStoppedException e) {
+            System.out.println("depth search interrupted");
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        
+        return this.bestMove;
+    }
+
+    // Maximizing fuction
+    // If initial, then this is the first (best) move to be calculated, this move will then be stored.
+    // (Calculating and storing the best move for every GameState as suggested in the assignment is inefficient, 
+    //  because you will only actually use the value of the first GameState.)
+    public int Alpha(DraughtsState s, int alpha, int beta, int depth, boolean initial) throws AIStoppedException {
+        if(stopped) throw new AIStoppedException();
+        
+        // This node is a leaf
+        List<Move> children = s.getMoves();
+        if(depth <= 0 || children.isEmpty()) {
+            return heuristics.calculateVal(s, depth);
+        }
+        
+        // The best move of the future
+        Move futureBestMove = null;
+        
+        int v = -INF;
+        for (Move move : children) {
+            s.doMove(move);
+            int result = Beta(s, v, beta, depth - 1);
+            if (result > v) {
+                if (initial) {
+                    futureBestMove = move;
+                }
+                v = result;
+            }
+            s.undoMove(move);
+
+            // If this condition holds then we can do pruning
+            if (v >= beta) {
+                // break out of the for loop and return v.
+                break;
+            }
+        }
+
+        // Best move can be determined when we have looked at all children (except pruned onces).
+        if (initial) {
+            this.bestMove = futureBestMove;
+        }
+        return v;
+    }
+
+    // Minimizing function
+    public int Beta(DraughtsState s, int alpha, int beta, int depth) throws AIStoppedException {
+        // This node is a leaf
+        List<Move> children = s.getMoves();
+        if(depth <= 0 || children.isEmpty()) {
+            return heuristics.calculateVal(s, depth);
+        }
+        
+        int v = INF;
+        for (Move move : children) {
+            s.doMove(move);
+            int result = Alpha(s, alpha, v, depth - 1, false);
+            if (result < v) {
+                v = result;
+            }
+            s.undoMove(move);
+
+            // If this condition holds then we can do pruning
+            if (alpha >= v) {
+                // break out of the for loop and return v.
+                break;
+            }
+
+        }
+        
+        return v;
+    }
+
+    @Override
+    public void stop() {
+        this.stopped = true;
+    }
+    
+    private static class AIStoppedException extends Exception {}
+}
---
+++ src/nl/tue/s2id90/group06/HeuristicDeterminatorOld.java
@@ -0,0 +1,38 @@
+package nl.tue.s2id90.group06;
+
+import nl.tue.s2id90.draughts.DraughtsState;
+import static nl.tue.s2id90.draughts.DraughtsState.*;
+
+/**
+ *
+ * @author Jordie Kolkman & Tijs Maas
+ */
+public class HeuristicDeterminatorOld {
+    private final boolean isWhite;
+            
+    public HeuristicDeterminatorOld(DraughtsState s) {
+        isWhite = s.isWhiteToMove();
+    }
+       
+    // Determine the heuristic value for a certain state s
+    public int calculateVal(DraughtsState s, int depth) {
+        int[] pieces = s.getPieces();
+        pieces[0] = depth; // Add depth to the first place of the array: the first place used to be 0 all the time.
+        
+        // This is where the actual heuristic is calculated.
+        int h = 0;
+        for (int i = 1; i < pieces.length; i++) {
+            int piece = pieces[i];
+
+            h += ((piece == WHITEPIECE) ? 2 : 0) - ((piece == BLACKPIECE) ? 1 : 0) + 
+                    ((piece == WHITEKING) ? 3 : 0) - ((piece == BLACKKING) ? 4 : 0);
+        }
+
+        // Turn the heuristic around if we are black
+        if (isWhite) {
+            return h;
+        } else {
+            return -h;
+        }
+    }
+}
