a02a8792cac7e85d4446c9d46efa522c77865c61
s128314 <s128314@S128314.campus.tue.nl>
2015-03-04 15:49:19 +0100

    improvements on heuristicdeterminator

--- src/nl/tue/s2id90/group06/HeuristicDeterminator.java
+++ src/nl/tue/s2id90/group06/HeuristicDeterminator.java
@@ -14,18 +14,35 @@ public class HeuristicDeterminator {
         isWhite = s.isWhiteToMove();
     }
        
+    public double SQ_LEFT(int c) {
+        //checks if a piece is on the left side of the board.
+        return (c % 10 == 6)
+                ? 1.5 : 1;
+                        
+    }
+    
+    public double SQ_RIGHT(int c) {
+        //checks if a piece is on the left side of the board.
+        return (c % 10 == 5)
+                ? 1.5 : 1;
+                        
+    }
+    
     // Determine the heuristic value for a certain state s
-    public int calculateVal(DraughtsState s, int depth) {
+    public double calculateVal(DraughtsState s, int depth) {
         int[] pieces = s.getPieces();
         pieces[0] = depth; // Add depth to the first place of the array: the first place used to be 0 all the time.
         
         // This is where the actual heuristic is calculated.
-        int h = 0;
+        double h = 0;
         for (int i = 1; i < pieces.length; i++) {
             int piece = pieces[i];
-
-            h += ((piece == WHITEPIECE) ? 2 : 0) - ((piece == BLACKPIECE) ? 1 : 0) + 
-                    ((piece == WHITEKING) ? 3 : 0) - ((piece == BLACKKING) ? 4 : 0);
+            
+            
+            
+            
+            h += SQ_LEFT(i) * SQ_RIGHT(i) * ((piece == WHITEPIECE) ? 1 : 0) - SQ_LEFT(i) * SQ_RIGHT(i) * ((piece == BLACKPIECE) ? 1 : 0) + 
+                    ((piece == WHITEKING) ? 5 : 0) - ((piece == BLACKKING) ? 5 : 0);
         }
 
         // Turn the heuristic around if we are black
