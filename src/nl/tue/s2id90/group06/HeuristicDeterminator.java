a98114643af2a328a02a335f5b277b6932563d75
s128314 <s128314@S128314.campus.tue.nl>
2015-03-05 02:07:19 +0100

    cleared all the needless code

--- src/nl/tue/s2id90/group06/HeuristicDeterminator.java
+++ src/nl/tue/s2id90/group06/HeuristicDeterminator.java
@@ -54,52 +54,6 @@ public class HeuristicDeterminator {
                         ? c + 6 : c + 5; // ODD ROW : EVEN ROW
     }
     
-    public double my_piece(int c, DraughtsState s, int depth) {
-        int[] pieces = s.getPieces();
-        pieces[0] = depth; // Add depth to the first place of the array: the first place used to be 0 all the time.
-        for (int i = 1; i < pieces.length; i++) {
-            int piece = pieces[i];
-        
-        if(isWhite){//when a piece is white and your player is white it returns c(maar neemt hij dan alle c waardes mee waarvoor dit geldt?)
-            if(piece == WHITEKING || piece == WHITEPIECE){
-                return c;
-            }
-        }
-           else if(isWhite == false){
-    if(piece == BLACKKING || piece == BLACKPIECE){
-        return c;
-    }
-    
-    }
-           else return 0;
-        }
-        //hier moet iets anders volgens mij maar dit begrijp ik dus niet echt
-        return c;
-    }
-    
-   public double SQ_SQUARE(int c, DraughtsState s, int depth, double my_piece) {
-        //checks if a diamond is formed by 4 pieces.
-        int[] pieces = s.getPieces();
-        
-        // TIJS: I did this somewhere because I needed to hash the array pieces. Otherwise this is irrelevant code.
-        pieces[0] = depth; // Add depth to the first place of the array: the first place used to be 0 all the time.
-                    //hier check ik of het eerste stuk wat je checked niet aan de zijkant zit
-        if(c % 10 != 5 || c % 10 != 6){
-            if(c % 10 < 6 && c % 10 != 0){//dit is een check of je in een even of oneven rij zit omdat bij oneven het iets anders moet
-                if((c + 5 == my_piece) && (c + 6 == my_piece) && (c + 10 == my_piece)){//+5 is het stuk links achter +6 recht etc.
-        return 1.5;
-            }
-                        
-        }
-            else if((c + 4 == my_piece) && (c + 5 == my_piece) && (c + 10 == my_piece)){
-        return 1.5;
-            }
-        
-    }
-        return 1;
-    }
-   
-   
    // With c, find rightbelow d, then leftbelow e, then leftabove f. 
    //  If (c, f, d, e) are our color, then we have a square. And then we return 1, otherwise 0. (maybe sum this in our loop?)
    //    c
@@ -109,9 +63,9 @@ public class HeuristicDeterminator {
    //    e
    //
    public double is_square(int c, int pieces[]) {
-       int d = SQ_RIGHT_BOTTOM(c);
-       int e = SQ_LEFT_BOTTOM(d);
-       int f = SQ_RIGHT_TOP(e);
+       int d = SQ_RIGHT_TOP(c);
+       int e = SQ_LEFT_TOP(d);
+       int f = SQ_RIGHT_BOTTOM(e);
        // c = SQ_LEFT_TOP(f)
        
        // convert all kings to draughts (this makes the final checking easier
@@ -157,7 +111,7 @@ public class HeuristicDeterminator {
             //for the kings it is not checked whether they are on the sides of the board or not since they have more movement freedom
             //and their is no real benifit for them to be at the sides.
             h += SQ_LEFT(i) * SQ_RIGHT(i) * ((piece == WHITEPIECE) ? 1 : 0) - SQ_LEFT(i) * SQ_RIGHT(i) * ((piece == BLACKPIECE) ? 1 : 0) + 
-                    ((piece == WHITEKING) ? 5 : 0) - ((piece == BLACKKING) ? 5 : 0);
+                    ((piece == WHITEKING) ? 5 : 0) - ((piece == BLACKKING) ? 5 : 0) + is_square(i, pieces);
             
         }
         
@@ -179,7 +133,7 @@ public class HeuristicDeterminator {
             
             
             h += - SQ_LEFT(i) * SQ_RIGHT(i) * ((piece == WHITEPIECE) ? 1 : 0) + SQ_LEFT(i) * SQ_RIGHT(i) * ((piece == BLACKPIECE) ? 1 : 0) -
-                    ((piece == WHITEKING) ? 5 : 0) + ((piece == BLACKKING) ? 5 : 0);
+                    ((piece == WHITEKING) ? 5 : 0) + ((piece == BLACKKING) ? 5 : 0)+ is_square(i, pieces);
         }
         if(WHITEPIECE == 0 & WHITEKING == 0){
                 h = h + 100;
@@ -188,29 +142,3 @@ public class HeuristicDeterminator {
         }
     }
 }
-
-/* public int SQ_LEFT_TOP(int c) {
-        //If already on the left or top, we can not select a left top, hence return 0;
-        // The second statement checks whether the row is odd/even (this matters, because the 'distance' between squares depends on this).
-        return (c % 10 == 6 || c <= 5)
-                ? 0 : ((c % 10 >= 1) && (c % 10 <= 5))
-                        ? c - 5 : c - 6; // ODD ROW : EVEN ROW
-    }
-
-    public int SQ_LEFT_BOTTOM(int c) {
-        return (c % 10 == 6 || c > 45)
-                ? 0 : ((c % 10 >= 1) && (c % 10 <= 5))
-                        ? c - 5 : c - 4; // ODD ROW : EVEN ROW
-    }
-
-    public int SQ_RIGHT_TOP(int c) {
-        return (c % 10 == 5 || c <= 5)
-                ? 0 : ((c % 10 >= 1) && (c % 10 <= 5))
-                        ? c - 6 : c - 5; // ODD ROW : EVEN ROW
-    }
-
-    public int SQ_RIGHT_BOTTOM(int c) {
-        return (c % 10 == 5 || c > 45)
-                ? 0 : ((c % 10 >= 1) && (c % 10 <= 5))
-                        ? c + 6 : c + 5; // ODD ROW : EVEN ROW
-    }*/
